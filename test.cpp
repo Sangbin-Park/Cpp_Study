a28a1c5bf2aec9980756ec78fe26cb5d8b89f37b
sangbin_park <79267299+Sangbin-Park@users.noreply.github.com>
2026-08-01 15:21:30 +0000

    1

--- test.cpp
+++ test.cpp
@@ -4,9 +4,9 @@ using namespace std;
  
 int main()
 {
+    int num=100;
     cout << "Hello, C++ World!" << endl;
     std::cout << "__cplusplus = " << __cplusplus << std::endl;
     // test 260731
- 
     return 0;
 }
